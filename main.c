a5d9af9508fbe751dcac0de8912ee20a8bea6648
Mohamed shawky💀💀 <mohamedxz2006xz@gmail.com>
2025-04-23 18:28:49 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 int main() {
-    int plane, passport_number, promocode,password,airline,login_sing_up,degree;
+    int plane, passport_number, promocode,password,airline,login_sing_up,degree,go_go_and_back;
     float main_price, price_after_discount;
-    char username[20];
-    void log_in(char,int,int);
-     void sign_up(char,int,int);
+    char username[20] , nationality[20] ,email[20];
 
 
 
-    printf("*******                    welcom :) \n");
+
+    printf("********                   welcom :) \n");
     printf(" *      *\n");
     printf("  *      *\n");
     printf("***        ********************************************\n");
@@ -24,23 +24,36 @@ int main() {
     printf("                                    *       *\n");
     printf("                                   *******\n");
 
-    printf(" sign up==> 1 \n   log_in==> 2\n");
+    printf("                  sign up==> 1    or    log_in==> 2\n");
     scanf("%d",&login_sing_up);
     switch(login_sing_up)
     {
     case 1 :
-        sign_up(username,password,passport_number);
+       printf("Enter your Username:\n");
+    scanf("%s", username);
+
+    printf("Enter your password:\n");
+    scanf("%d", &password);
+
+    printf("Enter your passport number:\n");
+    scanf("%d", &passport_number);
+
+    printf("Enter your nationality:\n");
+    scanf("%s", nationality);
+
+    printf("Enter your email:\n");
+    scanf("%s", email);
         break;
     case 2 :
-              printf("Enter your Username:\n");
+    printf("Enter your Username:\n");
     scanf("%s", username);
-    if(username=="mohamed")
+    if(strcmp(username,"mohamed_shawky") == 0)
        {
        printf(" ");
        }
     else
     {
-       printf("user name is not correct ...");
+       printf("user name is not correct ):");
      goto end;
     }
     printf("Enter your password:\n");
@@ -51,12 +64,12 @@ int main() {
        }
     else
     {
-    printf("password is not correct ...");
+    printf("password is not correct ): ");
      goto end;
     }
     printf("Enter your passport number:\n");
     scanf("%d", &passport_number);
-        break;
+    break;
     }
     printf("select an air line \n Egypt air lines ==> 1 \n Horus air lines ==> 2 \n Alex air lines ==> 3\n");
     scanf("%d",&airline);
@@ -64,32 +77,32 @@ int main() {
     {
         case 1:
         printf("Egypt air lines\n");
-    printf("plane 1  cairo ==> newyork  price 350$   time ==>9am to 11am \n");
-    printf("plane 2  cairo ==> hong kong  price 640$ time ==>10am to 11am\n");
-    printf("plane 3  cairo ==> spain price 900$ time ==>9am to 2am\n");
-    printf("plane 4  cairo ==> dubai price 780$ time ==>5pm to 11pm\n");
-    printf("plane 5  cairo ==> canada price 520$ time ==>9am to 3am\n\n");
+    printf("plane 1  cairo ==> newyork    price 350$   time ==>9am  to 11am \n");
+    printf("plane 2  cairo ==> hong kong  price 640$   time ==>10am to 11am\n");
+    printf("plane 3  cairo ==> spain      price 900$   time ==>9am  to 2am\n");
+    printf("plane 4  cairo ==> dubai      price 780$   time ==>5pm  to 11pm\n");
+    printf("plane 5  cairo ==> canada     price 520$   time ==>9am  to 3am\n\n");
     break;
         case 2:
          printf("Horus air lines\n");
-    printf("plane 6 luxor ==> newyork price 350$ time ==>4am to 11am\n");
-    printf("plane 7  luxor ==> hong kong price 640$ time ==>5am to 11am\n");
-    printf("plane 8  luxor ==> spain price 900$time ==>9pm to 8pm\n");
-    printf("plane 9 luxor ==> dubai price 780$ time ==>7am to 7am\n");
-    printf("plane 10  luxor ==> canada price 520$ time ==>6am to 11am\n\n");
+    printf("plane 6 luxor ==> newyork     price 350$   time ==>4am to 11am\n");
+    printf("plane 7  luxor ==> hong kong  price 640$   time ==>5am to 11am\n");
+    printf("plane 8  luxor ==> spain      price 900$   time ==>9pm to 8pm\n");
+    printf("plane 9 luxor ==> dubai       price 780$   time ==>7am to 7am\n");
+    printf("plane 10  luxor ==> canada    price 520$   time ==>6am to 11am\n\n");
     break;
          case 3:
            printf("Alex air lines\n");
-    printf("plane  11 alex ==> newyork price 350$ time ==>3am to 11pm\n");
-    printf("plane  12  alex ==> hong kong price 640$ time ==>7am to 2pm\n");
-    printf("plane  13  alex ==> spain price 900$ time ==>8am to 9am\n");
-    printf("plane  14  alex ==> dubai price 780$ time ==>3am to 1pm\n");
-    printf("plane  15  alex ==> canada price 520$ time ==>6am to 2am\n\n\n");
+    printf("plane  11 alex ==> newyork     price 350$   time ==>3am to 11pm\n");
+    printf("plane  12  alex ==> hong kong  price 640$   time ==>7am to 2pm\n");
+    printf("plane  13  alex ==> spain      price 900$   time ==>8am to 9am\n");
+    printf("plane  14  alex ==> dubai      price 780$   time ==>3am to 1pm\n");
+    printf("plane  15  alex ==> canada     price 520$   time ==>6am to 2am\n\n\n");
     break;
-    }
 
+    }
     printf("Enter the number of your plane :\n");
-    scanf("%d ",&plane);
+    scanf("%d",&plane);
     switch (plane)
      {
      case 1:
@@ -137,17 +150,31 @@ int main() {
      case 15:
         main_price=520;
         break;
+        default:
+    printf("Invalid plane number!\n");
+    goto end;
      }
-         printf("Enter the number of your degree ( 1 ==> local) ( 2 ==vip ):\n");
-    scanf("%f",&degree);
+    printf("Enter the number of your degree ( 1 ==> local) ( 2 ==vip ):\n");
+    scanf("%d",&degree);
     switch (degree)
      {
      case 2:
-        main_price+= 500;
+        main_price*= 2.9;
+        break;
      }
-
+    printf("          just go ==> 1     or    go and back ==> 2\n");
+    scanf("%d",&go_go_and_back);
+    switch(go_go_and_back)
+    {
+    case 1:
+        printf("");
+    break;
+    case 2:
+    main_price*=1.7;
+    break;
+    }
     printf("Enter promocode (press 0 if you don't have one):\n");
-    scanf("%d", &promocode);
+    scanf("%d",&promocode);
 
     switch(promocode) {
         case 0:
@@ -164,23 +191,11 @@ int main() {
         default:
             printf("Invalid promocode. The price remains %f\n", main_price);
     }
-    printf("your ticket has booked");
+    printf("your name is %s \n your passport number is %d",username,passport_number);
+    printf("your ticket has booked successfully (:");
     end:
-    return 0;
-}
-
-void sign_up(char username,int password,int passport_number)
-{
-    printf("Enter your Username:\n");
-    scanf("%s",&username);
-
-    printf("Enter your password:\n");
-    scanf("%d",&password);
+    return 0;}
 
 
-    printf("Enter your passport number:\n");
-    scanf("%d",&passport_number);
-
-}
 
 
